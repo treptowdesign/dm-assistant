ea1d04c9cfc808c615810a5139ecf3a909d93289
Kyle Treptow <kyletreptowdesign@gmail.com>
2025-03-02 17:31:31 -0800

    sass general styles

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
 import AuthNav from "@/app/components/AuthNav";
+
+import "@/app/general.sass"
 
 export default function Home() {
   return (
@@ -23,7 +24,7 @@ export default function Home() {
 
       </main>
 
-      
+
       <footer className={styles.footer}>
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
