8a43f03e43604db0dce3857b66efee678599c36a
Kyle Treptow <kyletreptowdesign@gmail.com>
2025-03-08 17:12:08 -0800

    textarea component scale size

--- src/app/components/inputs/TextareaInput.jsx
+++ src/app/components/inputs/TextareaInput.jsx
@@ -1,13 +1,24 @@
+import { useRef, useEffect } from "react";
+
 export default function TextareaInput({ label, name, value, onChange }) {
+    const textareaRef = useRef(null);
+    useEffect(() => {
+      if (textareaRef.current) {
+        textareaRef.current.style.height = "auto"; // reset height 
+        textareaRef.current.style.height = textareaRef.current.scrollHeight + "px"; // set to new height
+      }
+    }, [value]);
     return (
       <div>
         <label>{label}:</label>
         <textarea
+          ref={textareaRef}
+          style={{ width: "400px", resize: "none", overflow: "hidden", }}
           placeholder=""
           name={name} 
           value={value}
           onChange={(e) => onChange(name, e.target.value)}
-          rows={4}
+          rows={1}
         />
       </div>
     );
