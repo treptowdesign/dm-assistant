5f60c3e525e97a93f2c824fa401fe2cfdb1578a9
Kyle Treptow <kyletreptowdesign@gmail.com>
2025-03-16 11:41:42 -0700

    markdown utility - disable br for now

--- src/app/lib/markdown.js
+++ src/app/lib/markdown.js
@@ -3,7 +3,7 @@ export function simpleMarkdownToHtml(markdown) {
       .replace(/^(#+) (.*)/gm, (_, hashes, text) => `<h${hashes.length}>${text}</h${hashes.length}>`) // Headings
       .replace(/\*\*(.*?)\*\*/g, "<strong>$1</strong>") // Bold
       .replace(/\*(.*?)\*/g, "<em>$1</em>") // Italic
-      .replace(/`([^`]+)`/g, "<code>$1</code>") // Inline code
-      .replace(/\n/g, "<br>"); // Line breaks
+      .replace(/`([^`]+)`/g, "<code>$1</code>"); // Inline code
+      // .replace(/\n/g, "<br>"); // Line breaks (disabled for now, adds too many)
 }
   
