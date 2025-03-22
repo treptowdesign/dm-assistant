51a35674748847f6ce6fc1f06ad3816c79078bb3
Kyle Treptow <kyletreptowdesign@gmail.com>
2025-03-22 15:26:01 -0700

    campaign - disable server campaign form

--- src/app/campaigns/page.jsx
+++ src/app/campaigns/page.jsx
@@ -1,8 +1,8 @@
 "use server";
  
 import { getCampaigns } from "@/app/actions/campaigns/getCampaigns";
-import { createCampaign } from "@/app/actions/campaigns/createCampaign";
-import Form from "next/form";
+// import { createCampaign } from "@/app/actions/campaigns/createCampaign";
+// import Form from "next/form";
 import styles from "@/app/campaigns/styles.module.sass"; 
 import CampaignList from "./CampaignList"; // import Client Component
 import CampaignsFooter from "./CampaignsFooter"; // import Client Component footer (for <Link>)
@@ -19,15 +19,12 @@ export default async function CampaignsPage() {
 
         <NewCampaignForm />
 
-
-        <Form action={createCampaign} className={styles.cform}> 
+        {/* <Form action={createCampaign} className={styles.cform}> 
           <h2>Create New Campaign (server form):</h2>
           <input type="text" name="name" placeholder="Campaign Name" required />
           <textarea name="description" placeholder="Description" required />
           <button type="submit">Create Campaign</button> 
-        </Form>
-
-
+        </Form> */}
 
         <CampaignList campaigns={campaigns} />
 
